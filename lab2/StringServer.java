6c585529b9ec0e9f24c00470f13ff114ec64ec35
Jasper Huang <jasper.hyj@gmail.com>
2023-10-16 13:07:55 -0700

    update lab2

--- lab2/StringServer.java
+++ lab2/StringServer.java
@@ -1,1 +1,47 @@
+import java.io.IOException;
+import java.net.URI;
 
+class Handler implements URLHandler {
+
+  // The one bit of state on the server: a number that will be manipulated by
+  // various requests.
+  int string = "";
+  int count = 0;
+
+  public String handleRequest(URI url) {
+    if (url.getPath().equals("/")) {
+      return string;
+    } else if (url.getPath().equals("/add-message")) {
+      count += 1;
+      String[] parameters = url.getQuery().split("=");
+      if (parameters[0].equals("s")) {
+        string +=
+          String.format("%d. %s\n", count, String.parseString(parameters[1]));
+        return String.format(
+          "Number increased by %s! It's now %d",
+          parameters[1],
+          num
+        );
+      }
+      return String.format("Number incremented!");
+    } else {
+      return "404 Not Found!";
+    }
+  }
+}
+
+public class StringServer {
+
+  public static void main(String[] args) throws IOException {
+    if (args.length == 0) {
+      System.out.println(
+        "Missing port number! Try any number between 1024 to 49151"
+      );
+      return;
+    }
+
+    int port = Integer.parseInt(args[0]);
+
+    Server.start(port, new Handler());
+  }
+}
